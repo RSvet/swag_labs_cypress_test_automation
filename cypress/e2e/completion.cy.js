642413622954775b39ba3c6b7de4ad9ad4a6d352
Svetlana Radisic <svetlana_radisic@yahoo.com>
2025-09-11 17:53:54 +0200

    add tests for completion page ui and navigation

---
+++ cypress/e2e/completion.cy.js
@@ -0,0 +1,54 @@
+/// <reference types="cypress" />
+import pageData from "../fixtures/pageData"
+import credentials from "../fixtures/credentials"
+import checkoutInfo from "../fixtures/checkoutInfo"
+import productsData from "../fixtures/productsData"
+import LoginPage from "../pages/LoginPage"
+import ProductsPage from "../pages/ProductsPage"
+import Header from "../pages/Header"
+import CartPage from "../pages/CartPage"
+import CheckoutPage from "../pages/CheckoutPage"
+import OverviewPage from "../pages/OverviewPage"
+import CompletionPage from "../pages/CompletionPage"
+const loginPage = new LoginPage()
+const header = new Header()
+const productsPage = new ProductsPage()
+const cartPage = new CartPage()
+const checkoutPage = new CheckoutPage()
+const overviewPage = new OverviewPage()
+const completionPage = new CompletionPage()
+
+describe('Completion page tests', () => {
+  beforeEach(() => {
+    loginPage.navigateToLoginPage()
+    loginPage.login(credentials.validUsername, credentials.validPassword)
+  })
+  describe('Checkout Complete page display', () => {
+    it('TC-042: UI verification of "Checkout: Complete" page', () => {
+
+      productsPage.addDifferentProducts(2).then(() => {
+        header.openCart()
+        cartPage.clickOnCheckoutButton()
+        checkoutPage.populateInfoForm(checkoutInfo.validFirstName, checkoutInfo.validLastName, checkoutInfo.validZipCode)
+        checkoutPage.clickContinueButton()
+        overviewPage.clickFinishButton()
+        completionPage.verifyPageLoaded(pageData.completion)
+        completionPage.verifyVisibilityOfOrderCompletionText()
+      })
+    })
+  })
+
+  describe('Navigation from Complete page', () => {
+    it('TC-043: Validate navigation from the page', () => {
+      productsPage.addDifferentProducts(1).then(() => {
+        header.openCart()
+        cartPage.clickOnCheckoutButton()
+        checkoutPage.populateInfoForm(checkoutInfo.validFirstName, checkoutInfo.validLastName, checkoutInfo.validZipCode)
+        checkoutPage.clickContinueButton()
+        overviewPage.clickFinishButton()
+        completionPage.clickBackHomeButton()
+        productsPage.verifyAllProductsPresent(productsData.products.length)
+      })
+    })
+  })
+})
--- cypress/pages/CompletionPage.js
+++ cypress/pages/CompletionPage.js
@@ -4,4 +4,20 @@ import BasePage from "./BasePage";
 
 export default class CompletionPage extends BasePage {
 
+  //Locators
+
+  checkoutCompleteContainer = '#checkout_complete_container'
+  backHomeButton = '[data-test="back-to-products"]'
+  thankYouMsg = 'Thank you for your order!'
+  completeMsg = 'Your order has been dispatched, and will arrive just as fast as the pony can get there!'
+
+  verifyVisibilityOfOrderCompletionText() {
+    cy.get(this.checkoutCompleteContainer).should('contain', this.thankYouMsg)
+    cy.get(this.checkoutCompleteContainer).should('contain', this.completeMsg)
+  }
+
+  clickBackHomeButton() {
+    cy.get(this.backHomeButton).click()
+  }
+
 }
